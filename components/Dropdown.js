06f8603fab376b9861ec475e7518d7836fd34443
ipuhakka <iiropuhakka@luukku.com>
2022-03-03 18:30:41 +0200

    Adds add new handling to dropdown

--- components/Dropdown.js
+++ components/Dropdown.js
@@ -1,43 +1,82 @@
-import React from 'react';
-import { View } from 'react-native';
-import { Button, Menu, Provider } from 'react-native-paper';
+import React, {useState} from 'react';
+import { View, StyleSheet, Modal } from 'react-native';
+import { Title, Button, TextInput } from 'react-native-paper';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Dropdown } from 'react-native-element-dropdown';
 import PropTypes from 'prop-types';
 
-const Dropdown = ({ title, selections }) => {
-  const [visible, setVisible] = React.useState(false);
+const styles = StyleSheet.create({
+  dropdownContainer: {
+      flexDirection: 'row',
+      flex: 1,
+  },
+  dropdown: {
+    height: 50,
+    borderColor: 'gray',
+    borderWidth: 0.5,
+    borderRadius: 8,
+    paddingHorizontal: 8,
+    margin: 2
+  },
+  modalButtonContainer: {
+    flexDirection: 'row',
+    flex: 1
+  }
+});
 
-  const openMenu = () => setVisible(true);
+const DropdownWrapper = ({ title, options, allowAddNew, onAddNew }) => 
+{
+  const [modalVisible, setModalVisible] = useState(false);
 
-  const closeMenu = () => setVisible(false);
+  const [value, setValue] = useState(null);
+  const [newItem, setNewItem] = useState(null);
 
-  console.log('render', visible);
   return (
-    <Provider>
-      <View
-        style={{
-          flexDirection: 'row',
-          justifyContent: 'center',
-        }}>
-        <Menu
-          visible={visible}
-          onDismiss={closeMenu}
-          anchor={<Button mode='outlined' color='blue' onPress={openMenu}>{title}</Button>}>
-          {selections.map((selection, i) => 
-            <Menu.Item
-                onPress={() => console.log('TODO: Onpress', selection)}
-                title={selection.title}/>)}
-        </Menu>
-      </View>
-    </Provider>
+    <View>
+      <Modal
+        visible={modalVisible}>
+          <TextInput label='Uusi' onChangeText={(newText) => setNewItem(newText)} value={newItem}/>
+          <View style={styles.modalButtonContainer}>
+            <Button color='black' onPress={() => setModalVisible(false)}>Peruuta</Button>
+            <Button 
+              color='blue'
+              onPress={() => 
+              {
+                onAddNew(newItem);
+                setNewItem(null);
+              }}>Lisää</Button>
+          </View>
+        </Modal>
+      <Title>{title}</Title>
+      <Dropdown
+        style={[styles.dropdown]}
+        search={options.length > 10}
+        data={options}
+        title={title}
+        labelField="title"
+        valueField="key"
+        placeholder={title}
+        value={value}
+        onChange={item => 
+        {
+          setValue(item.key);
+          console.log('change', item);
+        }} />
+      {allowAddNew && <Button
+        color='blue'
+        onPress={() => setModalVisible(true)}>Lisää uusi</Button>}
+    </View>
   );
 };
 
 Dropdown.propTypes = {
     title: PropTypes.string.isRequired,
-    selections: PropTypes.arrayOf(PropTypes.shape({
+    options: PropTypes.arrayOf(PropTypes.shape({
         key: PropTypes.any,
         title: PropTypes.string
-    })).isRequired
+    })),
+    allowAddNew: PropTypes.bool,
+    onAddNew: PropTypes.func
 }
 
-export default Dropdown;
+export default DropdownWrapper;
