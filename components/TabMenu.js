dd0d66fcde78dc0453292c5e18e493f51e4eaf6b
ipuhakka <iiropuhakka@luukku.com>
2022-03-15 19:17:49 +0200

    Implements basic tab menu

---
+++ components/TabMenu.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import { ScrollView, View, StyleSheet} from 'react-native'
+import { Button } from 'react-native-paper';
+import PropTypes from 'prop-types';
+
+const styles = StyleSheet.create({
+    tabMenu: {
+        flexDirection: 'row',
+        borderColor: 'black',
+        borderBottomWidth: 1
+    }
+});
+
+const TabMenu = ({panes}) =>
+{
+    const [activeTab, setActiveTab] = useState(0);
+
+    const menuButtons = panes.map((pane, i) => 
+        <Button 
+            key={`tab-menu-item-${i}`}
+            onPress={() => setActiveTab(i)}
+            color={i === activeTab ? 'black' : 'blue'}>{pane.title}</Button>);
+
+    return <View>
+        <ScrollView 
+            style={styles.tabMenu}
+            horizontal>
+            {menuButtons}
+        </ScrollView>
+        <ScrollView>{panes.length > 0
+            ? panes[activeTab].content
+            : null}</ScrollView>
+    </View>;
+}
+
+TabMenu.propTypes = {
+    panes: PropTypes.arrayOf(PropTypes.shape({
+        title: PropTypes.string.isRequired,
+        content: PropTypes.any
+    }))
+};
+
+export default TabMenu;
